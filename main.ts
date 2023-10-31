6166b18ef9216415a94b82253cb762559abc219e
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-10-31 13:23:39 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,28 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Daki A.B
+ * Created on: Oct 2023
+ * This program counts down
 */
 
-basic.showString('Hello, World!')
+// variables
+let countDown : number = 4
+let neopixelStrip : neopixel.Strip = null
+
+// start up
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+  while (countDown > -1) {
+    basic.showNumber(countDown)
+    countDown --
+}
+
+})
+
+// on button B, clearing screen
+input.onButtonPressed(Button.B, function () {
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+})
