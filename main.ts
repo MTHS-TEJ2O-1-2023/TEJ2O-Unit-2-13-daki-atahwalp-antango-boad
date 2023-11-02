5c186fc7341e9774272eedae406a4d164839830b
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-11-02 13:08:30 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -12,18 +12,25 @@ let countDown: number = 4
 // start up
 basic.clearScreen()
 neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Violet))
-neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Violet))
-neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Violet))
-neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Violet))
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
 // on button A, counting down
 input.onButtonPressed(Button.A, function () {
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Purple))
+  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Purple))
+  neopixelStrip.show()
+
   while (countDown > -1) {
     basic.showNumber(countDown)
     countDown--
+    // counting down with neopixel
     neopixelStrip.setPixelColor(countDown, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.show()
   }
