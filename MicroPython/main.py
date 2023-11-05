4b5d0acd807a723d3545a3771231bbf294b281f4
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-11-05 15:56:36 +0000

    works

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,41 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Daki A.B
+Created on: Nov 3
+This module is a Micro:bit MicroPython program that counts down from 4 
 """
 
 from microbit import *
+import neopixel
 
+np = neopixel.NeoPixel(pin16, 4)
+count_down = 4
 
-display.scroll("Hello, World!")
+display.clear()
+print(np[0])
+np[0] = (0, 0, 0)
+print(np[1])
+np[1] = (0, 0, 0)
+print(np[2])
+np[2] = (0, 0, 0)
+print(np[3])
+np[3] = (0, 0, 0)
+np.show()
+display.show(Image.HAPPY)
+
+if button_a.is_pressed():
+    display.clear()
+    print(np[0])
+    np[0] = (128, 0, 128)
+    print(np[1])
+    np[1] = (128, 0, 128)
+    print(np[2])
+    np[2] = (128, 0, 128)
+    print(np[3])
+    np[3] = (128, 0, 128)
+    np.show()
+
+while count_down > -1:
+    display.show(count_down)
+    count_down -= 1
+    np[count_down] = (0, 0, 0)
+    np.show()
